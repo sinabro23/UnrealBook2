a6ea951103aa2fccfc302d035645dbb756ee2b66
jinabro <kmy4869@naver.com>
2022-03-04 01:23:17 +0900

    첫 커밋

---
+++ UnrealBook2/Source/UnrealBook2.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UnrealBook2Target : TargetRules
+{
+	public UnrealBook2Target( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "UnrealBook2" } );
+	}
+}
---
+++ UnrealBook2/Source/UnrealBook2Editor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class UnrealBook2EditorTarget : TargetRules
+{
+	public UnrealBook2EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "UnrealBook2" } );
+	}
+}
